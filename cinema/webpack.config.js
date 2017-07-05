5b09785cfc6c179713f55eecbf587f8243a5c0c0
张丹 <189875670@qq.com>
2017-07-05 17:02:41 +0800

    第一次提交 css兼容已配完,下一步配置实时刷新

---
+++ cinema/webpack.config.js
@@ -0,0 +1,39 @@
+var htmlWebpackPlugin = require("html-webpack-plugin");
+var extractTextPlugin = require("extract-text-webpack-plugin");
+module.exports={
+	entry:{
+		index:"./src/js/index.js"
+	},
+	output:{
+		filename:"js/[name]-bundle.js",
+		path:__dirname+"/public",
+		publicPath:"http://localhost:3000"
+	},
+	module:{
+		rules:[
+			{
+				test:/\.css$/,
+				loader: extractTextPlugin.extract({
+		          	fallback: "style-loader",
+		          	use: "css-loader!postcss-loader"
+		        })
+			},
+			{
+				test:/\.js$/,
+				loader:"babel-loader"
+			},
+			{
+				test:/\.html/,
+				loader:"html-loader"
+			}			
+		]
+	},
+	plugins:[
+		new extractTextPlugin("css/[name].css"),
+		new htmlWebpackPlugin({
+			filename:"../views/index.html",
+			template:"./src/tpl/index.html",
+			chunks:["index"]
+		})
+	]
+}
