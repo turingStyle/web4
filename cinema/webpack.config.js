aed834ef43dc823769178b6f08978d95602cb4c1
张丹 <189875670@qq.com>
2017-07-06 15:56:45 +0800

    第三次提交,webpack搭建已完成

--- cinema/webpack.config.js
+++ cinema/webpack.config.js
@@ -1,5 +1,13 @@
 var htmlWebpackPlugin=require('html-webpack-plugin');
 var extractTextPlugin=require('extract-text-webpack-plugin');
+
+var fs = require('fs');
+var tplPath='./src/tpl';
+var files = fs.readdirSync(tplPath);
+
+var entry =setEntry(files);
+var plugins=setPlugins(files);
+
 module.exports={
 	entry:{
 		main:'./src/js/index.js'
@@ -44,4 +52,30 @@ module.exports={
 			chunks:['main']
 		})
 	]
+}
+
+function setEntry(files){
+	var entry={}
+	for(var i=0;i<files.length;i++){
+		var entryKey = files[i].substring(0,files[i].indexOf('.'));
+		console.log(entryKey);
+		entry[entryKey]='./src/js/'+entryKey+'.js';
+	}
+	return entry;
+}
+
+
+function setPlugins(files){
+	var plugins=[new extractTextPlugin('css/[name].css')];
+	for(var i=0;i<files.length;i++){
+		var entryKey = files[i].substring(0,files[i].indexOf('.'));
+		plugins.push(
+			new htmlWebpackPlugin({
+				filename:'../views/'+entryKey+'.html',
+				template:'./src/tpl/'+entryKey+'.html',
+				chunks:[entryKey]
+			})
+		)
+	}
+	return plugins;
 }
