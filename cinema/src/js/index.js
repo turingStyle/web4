3e6518568d3d1687d0953437bc8a4aa0e7c5fa6a
张丹 <189875670@qq.com>
2017-07-07 16:54:15 +0800

    第五次提交,首页 登录 注册已完成

--- cinema/src/js/index.js
+++ cinema/src/js/index.js
@@ -1,6 +1,9 @@
 import '../css/index.css';
+import '../css/swiper-3.4.2.min.css';
 import './hmr.js';
 import 'jquery';
+import './swiper-3.4.2.min.js';
+import './angular.min.js';
 import 'bootstrap-loader';
 import 'bootstrap';
 
